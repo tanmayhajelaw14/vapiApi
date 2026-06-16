4f63f0ce76d14ce94a968a56fb92c5abe6627ef3
ryomenskuna <mevishuth14@gmail.com>
2026-06-16 19:24:32 +0530

    configured localhost via ngrok and settled it with vapi

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, request
+from dataclasses import dataclass
+import marshmallow_dataclass
+
+app = Flask(__name__)
+
+@dataclass
+class Message:
+    type: str
+    transcript: str | None = None
+
+MessageSchema = marshmallow_dataclass.class_schema(Message)
+
+@app.route("/webhook", methods=["POST"])
+def webhook():
+
+    message = MessageSchema(unknown="exclude").load(request.json["message"])
+
+    print("\nEvent Type:", message.type)
+
+    if message.transcript:
+        print("Transcript:", message.transcript)
+
+    return {"success": True}
+
+app.run(port=5000)
